b8e2f6f50af28c7ca256a41f20dffe10e670be33
Maryvil Sandoval <42069512+mvillsan@users.noreply.github.com>
2022-06-07 22:30:10 +0800

    Add files via upload

---
+++ Admin/Client/Pages/AddEditCustomer.razor.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Components;
+using Admin.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using Microsoft.JSInterop;
+
+namespace Admin.Client.Pages
+{
+    public class AddEditCustomerModel : ComponentBase
+    {
+        [Inject]
+        protected HttpClient Http { get; set; }
+        [Inject]
+        public NavigationManager urlNavigationManager { get; set; }
+        [Parameter]
+        public string custID { get; set; }
+        protected string Title = "Add";
+        public Customer cust = new Customer();
+
+        protected override async Task OnParametersSetAsync()
+        {
+            if (!string.IsNullOrEmpty(custID))
+            {
+                Title = "Edit";
+                cust = await Http.GetFromJsonAsync<Customer>("api/Customer/" + custID);
+            }
+        }
+
+        protected async Task SaveCustomer()
+        {
+            if (cust.cust_id != null)
+            {
+                await Http.PutAsJsonAsync("api/Customer", cust);
+            }
+
+            else
+            {
+                await Http.PostAsJsonAsync("api/Customer", cust);
+            }
+
+            Cancel();
+        }
+
+        public void Cancel()
+        {
+            urlNavigationManager.NavigateTo("/customerrecords");
+        }
+    }
+}
---
+++ Admin/Client/Pages/DeleteCustomer.razor.cs
@@ -0,0 +1,44 @@
+﻿using Admin.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Admin.Client.Pages
+{
+    public class DeleteCustomerModel:ComponentBase
+    {
+        [Inject]
+        protected HttpClient Http { get; set; }
+        [Inject]
+        public NavigationManager urlNavigationManager { get; set; }
+        [Parameter]
+        public string custID { get; set; }
+        protected string Title = "Delete";
+        public Customer cust = new Customer();
+
+        protected override async Task OnParametersSetAsync()
+        {
+            if (!string.IsNullOrEmpty(custID))
+            {
+                Title = "Confirm Delete";
+                cust = await Http.GetFromJsonAsync<Customer>("api/Customer/" + custID);
+            }
+        }
+
+        protected async Task DeleteCustomer()
+        {
+
+            await Http.DeleteAsync("api/Customer/" + custID);
+            Cancel();
+        }
+
+        public void Cancel()
+        {
+            urlNavigationManager.NavigateTo("/customerrecords");
+        }
+    }
+}
