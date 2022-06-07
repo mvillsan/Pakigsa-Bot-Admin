42130c5f366502ffc6f1cf9d6b6d0133b5a758c5
Maryvil Sandoval <42069512+mvillsan@users.noreply.github.com>
2022-06-07 22:19:15 +0800

    Add files via upload

---
+++ Client/Pages/AddEditEstablishment.razor.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components;
+using Admin.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+
+namespace Admin.Client.Pages
+{
+    public class AddEditEstablishmentModel : ComponentBase
+    {
+        [Inject]
+        protected HttpClient Http { get; set; }
+        [Inject]
+        public NavigationManager urlNavigationManager { get; set; }
+        [Parameter]
+        public string estID { get; set; }
+        protected string Title = "Add";
+        public Establishment est = new Establishment();
+
+        protected override async Task OnParametersSetAsync()
+        {
+            if (!string.IsNullOrEmpty(estID))
+            {
+                Title = "Edit";
+                est = await Http.GetFromJsonAsync<Establishment>("api/Establishment/" + estID);
+            }
+        }
+
+        protected async Task SaveEstablishment()
+        {
+            if (est.est_id != null)
+            {
+                await Http.PutAsJsonAsync("api/Establishment", est);
+            }
+
+            else
+            {
+                await Http.PostAsJsonAsync("api/Establishment", est);
+            }
+
+            Cancel();
+        }
+
+        public void Cancel()
+        {
+            urlNavigationManager.NavigateTo("/establishmentrecords");
+        }
+    }
+}
---
+++ Client/Pages/DeleteEstablishment.razor.cs
@@ -0,0 +1,43 @@
+﻿using Admin.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Admin.Client.Pages
+{
+    public class DeleteEstablishmentModel : ComponentBase
+    {
+        [Inject]
+        protected HttpClient Http { get; set; }
+        [Inject]
+        public NavigationManager urlNavigationManager { get; set; }
+        [Parameter]
+        public string estID { get; set; }
+        protected string Title = "Delete Establishment";
+        public Establishment est = new Establishment();
+
+        protected override async Task OnParametersSetAsync()
+        {
+            if (!string.IsNullOrEmpty(estID))
+            {
+                Title = "Confirm Delete";
+                est = await Http.GetFromJsonAsync<Establishment>("api/Establishment/" + estID);
+            }
+        }
+
+        protected async Task DeleteEstablishment()
+        {
+            await Http.DeleteAsync("api/Establishment/" + estID);
+            Cancel();
+        }
+
+        public void Cancel()
+        {
+            urlNavigationManager.NavigateTo("/establishmentrecords");
+        }
+    }
+}
